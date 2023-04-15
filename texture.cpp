cc2c49caf6d7177bde5626c30defad8482371eed
kinpowoo <kinpowoo@outlook.com>
2023-04-15 18:02:14 +0800

    first commit

---
+++ texture.cpp
@@ -0,0 +1,47 @@
+/*******************************************************************
+** This code is part of Breakout.
+**
+** Breakout is free software: you can redistribute it and/or modify
+** it under the terms of the CC BY 4.0 license as published by
+** Creative Commons, either version 4 of the License, or (at your
+** option) any later version.
+******************************************************************/
+#include <iostream>
+
+#include "texture.h"
+
+
+Texture2D::Texture2D()
+    : Width(0), Height(0), Internal_Format(GL_RGB), Image_Format(GL_RGB), Wrap_S(GL_REPEAT), Wrap_T(GL_REPEAT), Filter_Min(GL_LINEAR), Filter_Max(GL_LINEAR)
+{
+    glGenTextures(1, &this->ID);
+}
+
+void Texture2D::Generate(GLuint width, GLuint height, unsigned char* data,GLuint nrComponents)
+{
+    this->Width = width;
+    this->Height = height;
+    GLenum format;
+    if (nrComponents == 1)
+        format = GL_RED;
+    else if (nrComponents == 3)
+        format = GL_RGB;
+    else if (nrComponents == 4)
+        format = GL_RGBA;
+
+    // Create Texture
+    glBindTexture(GL_TEXTURE_2D, this->ID);
+    glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+    // Set Texture wrap and filter modes
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, this->Wrap_S);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, this->Wrap_T);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, this->Filter_Min);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, this->Filter_Max);
+    // Unbind texture
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+void Texture2D::Bind() const
+{
+    glBindTexture(GL_TEXTURE_2D, this->ID);
+}
